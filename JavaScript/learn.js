dfefe023d07ccd1b71ccf39173ece0150303fdac
mohana0304 <kmohanapriya0304@gmail.com>
2026-03-16 18:52:57 +0530

    Update code

---
+++ JavaScript/learn.js
@@ -0,0 +1,23 @@
+            "use strict";
+            /*m=10;
+            console.log(m);
+            if(true){
+                var x=true;
+            }
+            alert(x);
+
+            function myfun(){
+                if(true){
+                var x=true;
+            }
+            alert(x);
+            }
+            alert(x);*/
+            let m=10;
+            let str='Welcome';
+            let str1='Mohanapriya';
+            let n=`hi ${str1}`;
+            console.log(str);
+            console.log(n);
+            let a=10,b=300;
+            console.log( `the result is ${a + b}` ); 
